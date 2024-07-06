881825bec1ea9bdebf7aa43768b641ef38812280
Jose Ruiz <jruiz170.business@gmail.com>
2024-07-06 01:21:19 -0400

    init

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+
+const SPEED = 7.5
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		$Body.rotation.y = atan2(-velocity.x, -velocity.z)
+		$AnimationPlayer.play("Walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		$AnimationPlayer.play("Idle")
+
+	move_and_slide()
